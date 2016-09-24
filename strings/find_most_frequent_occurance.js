bbec128d95c9d798f457834490d24feeafbf7b64
jessiewuwu <jwu004@gmail.com>
2016-09-24 15:37:12 -0700

    Add time and space complexity and lowercase the string

--- strings/find_most_frequent_occurance.js
+++ strings/find_most_frequent_occurance.js
@@ -1,6 +1,9 @@
 /*
 Given a string, return the character that appears the most.
 
+Time:
+Space: 
+
 1. Instantiate a hash variable. 
 2. Iterate through the string
 3. If the character exists in the hash, increment the value;
@@ -9,18 +12,18 @@ Given a string, return the character that appears the most.
 6. Iterate through the hash comparing the values
 7. Set the first key and value in the result array and replace it every time there's a greater value
 8. Return the character from the result array 
-
 */
 
 var mostFreqOccurance = function(str) {
+  var cleanStr = str.toLowerCase();
   var hash = {};
   var result = [];
 
-  for(var i = 0; i < str.length; i++) {
-    if(hash[str[i]]) {
-      hash[str[i]]++;
+  for(var i = 0; i < cleanStr.length; i++) {
+    if(hash[cleanStr[i]]) {
+      hash[cleanStr[i]]++;
     } else {
-      hash[str[i]] = 1;
+      hash[cleanStr[i]] = 1;
     }
   }
 
@@ -37,4 +40,5 @@ var mostFreqOccurance = function(str) {
   return result[0];
 }
 
-console.log(mostFreqOccurance("kisseeeees"));
+console.log(mostFreqOccurance("kissEEEees"));
+console.log("HI".toLowerCase())
